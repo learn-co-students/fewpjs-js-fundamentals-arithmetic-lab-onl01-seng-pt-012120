a11a136c2409cdc3f10e77d8c5ab98ddf007b72b
Engineer-Amgad <amgadmalik@yahoo.com>
2020-07-28 10:25:44 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.ceil(Math.random() * 10) + 1;
+
+let mod = rem();
+
+function rem(){
+    return 24 % 10;
+}
+
+let max = Math.max(10, 20);
