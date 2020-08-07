11ca3604b3cbd6cd6574eea322eab890d066d569
LeonorPDX <leonoracolbert@gmail.com>
2020-08-07 14:04:50 -0700

    Done.

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let num1 = 1
 let num2 = 62
+
 const multiply = num1*num2
 
-const random = Math.floor(Math.random() * 10) + 1
+const random = Math.floor(Math.random() * 10000) + 1
 
 const mod = 9 % 5
 
