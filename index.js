57ef88e82a70b29bcdfefbdbc20f58d75abaeb5c
Starcatch <timurmishiev@gmail.com>
2020-08-15 01:18:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 
-// Write your code here
+// multiply
+// is an equation whose multiplied results will equal 62
+const num1 = 2;
+const num2 = 31;
+const multiply = num1 * num2;
+
+
+//random
+//is generates a random integer greater than 0
+
+const random = Math.floor(Math.random() * 10 + 1);
+
+// mod 
+// generates a remainder that is equal to 4
+
+let mod = 9 % 5;
+
+//   max
+// will return 20 as the highest number in the set
+
+let max = Math.max(1, 2, 3, 4, 20); 
+
